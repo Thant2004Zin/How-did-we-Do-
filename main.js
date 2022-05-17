951a5db48c2178fc3070ae4ed64b1a1ef0cb3f6c
Thant2004Zin` <frequency307@gmail.com>
2022-05-17 15:18:22 +0630

    What did we do

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.scss'
+import 'atropos/css'
+// import 'bootstrap/scss/bootstrap.scss'
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'animate.css'
+import Atropos from 'atropos';
+
+const myAtropos = Atropos({
+    el: '.my-atropos',
+    // rest of parameters
+});
