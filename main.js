7960c51bfe60be241478a7cb226658e573fbfcf4
Thant2004Zin` <frequency307@gmail.com>
2022-05-17 15:46:04 +0630

    ta kukuk

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ const myAtropos = Atropos({
     el: '.my-atropos',
     // rest of parameters
 });
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px; z-index : 1020;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500);
+    if (innerWidth < 700){
+        cursor.classList.remove("cursor")
+    }
+})
